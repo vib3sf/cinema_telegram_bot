60d0912ef64d01199a0c26227386c39befc61d7a
vib3sf <velobaton228@gmail.com>
2023-06-02 20:17:13 +0300

    add arg for get title with re pattern and raising exceptions

--- get_random_film.py
+++ get_random_film.py
@@ -1,30 +1,35 @@
 from bs4 import BeautifulSoup
 import requests
 import random
+import re
 
 
-def get_random_film(url, tag_title, class_title):
+def get_random_film(url, tag_title, class_title, pattern=''):
     page = requests.get(url)
 
     if not page.status_code == 200:
-        print('Something went wrong')
-        return None
+        raise requests.ConnectionError('Something went wrong.')
 
     soup = BeautifulSoup(page.text, 'html.parser')
     films = soup.findAll(tag_title, class_title)
 
-    while True:
-        if films:
-            film = random.choice(films).text
-            print(f'Your film: {film}. Want another?')
-            if not input() == 'yes':
-                films.remove(film)
-                return film
-        else:
-            print('Films are over :(')
-            return None
+    if films:
+        film = random.choice(films).text
+        if pattern:
+            match = re.search(pattern, film)
+            if film:
+                film = match.group(1)
+            else:
+                raise ValueError("Pattern isn`t found in title.")
+
+        return film
 
 
 if __name__ == '__main__':
-    get_random_film('https://www.kinoafisha.info/rating/movies/', 'a', 'movieItem_title')
+    while True:
+        film = get_random_film('https://www.timeout.com/film/best-movies-of-all-time',
+                        'h3', '_h3_cuogz_1', pattern=r'\d+\.\s+(.*)')
+        print(f'Your film: {film}. Want another?')
+        if input() != 'yes':
+            break
 
