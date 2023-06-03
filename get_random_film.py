92b5bdcfa315fa1116926fe0d2a2c108274775ac
vib3sf <velobaton228@gmail.com>
2023-06-03 15:11:56 +0300

    select title from db

--- get_random_film.py
+++ get_random_film.py
@@ -1,11 +1,16 @@
-#!/usr/bin/env python
 from collect_films import collect_films
 from textwrap import dedent
 from random import choice
+import sqlite3
 
 
 def get_random_film():
-    return f'Ваш фильм: {choice(collect_films())}'
+    connection = sqlite3.connect('films.db')
+    cur = connection.cursor()
+    cur.execute('SELECT title FROM films ORDER BY RANDOM() LIMIT 1;')
+    film = cur.fetchone()[0]
+    connection.close()
+    return f'Your film - {film}'
 
 
 if __name__ == '__main__':
